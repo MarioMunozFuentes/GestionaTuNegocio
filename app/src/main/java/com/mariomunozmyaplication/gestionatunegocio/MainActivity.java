17da9df2598a60efe42a7bb3070e8d754d29e41d
Asier97am <asier.aranda.malagon@gmail.com>
2022-04-25 12:54:43 +0200

    CREADO MENU LATERAL CON ICONOS Y STRING

--- app/src/main/java/com/mariomunozmyaplication/gestionatunegocio/MainActivity.java
+++ app/src/main/java/com/mariomunozmyaplication/gestionatunegocio/MainActivity.java
@@ -2,14 +2,143 @@ package com.mariomunozmyaplication.gestionatunegocio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.mariomunozmyaplication.gestionatunegocio.home.HomeFragment;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
+
+    public FloatingActionButton fab;
+    private Intent intent;
+
+
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+    //variable del fragment detalle
+   // MyApplication myApplication= (MyApplication) this.getApplication();
+
+    //AÑADIR DATOS
+    private ActivityResultLauncher<Intent> miActivityResultLauncher;
+    //private DatosRegistro persona;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //fab = findViewById(R.id.fab);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.nav_view);
+        //lo sgt se implementa luego de haber implementado NavigationView.OnNavigationItemSelectedListener
+        navigationView.setNavigationItemSelectedListener(this);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
+
+        //cargar fragment principal en la actividad
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.add(R.id.container_fragment, new HomeFragment());
+        fragmentTransaction.commit();
+
+
     }
 
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        //para cerrar automaticamente el menu
+        drawerLayout.closeDrawer(GravityCompat.START);
+        int id = menuItem.getItemId();
+        menuItem.setChecked(true);
+        switch (id) {
+            case R.id.home:
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.container_fragment, new HomeFragment());
+                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
+                fragmentTransaction.commit();
+                break;
+            case R.id.productos:
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.container_fragment, new HomeFragment());
+                Toast.makeText(this, "Productos", Toast.LENGTH_SHORT).show();
+                fragmentTransaction.commit();
+                menuItem.setChecked(true);
+                break;
+            case R.id.pedidos:
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.container_fragment, new HomeFragment());
+                Toast.makeText(this, "Pedidos", Toast.LENGTH_SHORT).show();
+                fragmentTransaction.commit();
+                menuItem.setChecked(true);
+                break;
+            case R.id.empleados:
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.container_fragment, new HomeFragment());
+                Toast.makeText(this, "Empleados", Toast.LENGTH_SHORT).show();
+                fragmentTransaction.commit();
+                break;
+            case R.id.Datos:
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.container_fragment, new HomeFragment());
+                Toast.makeText(this, "Datos de Usuario", Toast.LENGTH_SHORT).show();
+                fragmentTransaction.commit();
+                break;
+            case R.id.contacto:
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.container_fragment, new HomeFragment());
+                Toast.makeText(this, "Contacto", Toast.LENGTH_SHORT).show();
+                fragmentTransaction.commit();
+                break;
+            case R.id.salir:
+                intent = new Intent(this, HomeFragment.class);
+                Toast.makeText(this, "Saliendo", Toast.LENGTH_SHORT).show();
+                startActivity(intent);
+                finish();
+                break;
+            default:
+                return true;
+        }
+        return true;
+    }
 }
