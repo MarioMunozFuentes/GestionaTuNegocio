0a8895f9d42b6283012e3a62483ce202af23cd0e
Mario Muñoz Fuentes <mario.munoz.fuentes>
2022-04-25 10:16:47 +0200

    añadir mejoras

---
+++ app/src/main/java/com/mariomunozmyaplication/gestionatunegocio/Bienvenida.java
@@ -0,0 +1,13 @@
+package com.mariomunozmyaplication.gestionatunegocio;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Bienvenida extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bienvenida);
+    }
+}
