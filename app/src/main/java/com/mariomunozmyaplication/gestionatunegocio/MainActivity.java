3024f9bc5f22e97e2c7c0048d5169b1763f11192
Asier97am <asier.aranda.malagon@gmail.com>
2022-04-29 14:07:57 +0200

    ERROR FIREBASE

--- app/src/main/java/com/mariomunozmyaplication/gestionatunegocio/MainActivity.java
+++ app/src/main/java/com/mariomunozmyaplication/gestionatunegocio/MainActivity.java
@@ -1,7 +1,5 @@
 package com.mariomunozmyaplication.gestionatunegocio;
 
-
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,14 +8,12 @@ import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -25,7 +21,6 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
-import com.mariomunozmyaplication.gestionatunegocio.home.HomeFragment;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
