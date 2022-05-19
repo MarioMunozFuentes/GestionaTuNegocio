02838eccd83dc6145bf1d2a9239be4a800b4187d
Asier97am <asier.aranda.malagon@gmail.com>
2022-05-19 12:54:02 +0200

    Crear contactos

---
+++ app/src/main/java/com/mariomunozmyaplication/gestionatunegocio/ui/contacto/ContactosFragment.java
@@ -0,0 +1,32 @@
+package com.mariomunozmyaplication.gestionatunegocio.ui.contacto;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mariomunozmyaplication.gestionatunegocio.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ContactosFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ContactosFragment extends Fragment {
+
+    public ContactosFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_contactos, container, false);
+
+
+    }
+}
