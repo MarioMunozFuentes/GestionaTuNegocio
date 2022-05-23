edccc3bc4812a17e203f80aadc5916ab84ba5cdc
Mario Muñoz Fuentes <mario.munoz.fuentes>
2022-05-23 17:50:44 +0200

    APP "acabada"

--- app/src/main/java/com/mariomunozmyaplication/gestionatunegocio/ContactosActivity.java
+++ app/src/main/java/com/mariomunozmyaplication/gestionatunegocio/ContactosActivity.java
@@ -1,12 +1,7 @@
 package com.mariomunozmyaplication.gestionatunegocio;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
@@ -14,10 +9,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
-import com.google.android.material.snackbar.Snackbar;
 
 public class ContactosActivity extends AppCompatActivity {
 
@@ -48,10 +44,10 @@ public class ContactosActivity extends AppCompatActivity {
                 nombreEmpresa=tvNombreEmpresa.getText().toString();
                 sendIntent.putExtra(Intent.EXTRA_TEXT, "DATOS EMPRESA: "+
                         "\n\n" +
-                        "EMAIL: "+email+"\n"+
-                        "NOMBRE EMPRESA: "+ nombreEmpresa+" \n"+
-                        "DIRECCIÓN: "+"añadir dirección aquí \n"+
-                        "TELÉFONO: "+"añadir teléfono aquí");
+                        "EMAIL: "+email+"\n\n"+
+                        "NOMBRE EMPRESA: "+ nombreEmpresa); //+" \n" +
+                      // "DIRECCIÓN: "+"añadir dirección aquí \n"+
+                      //  "TELÉFONO: "+"añadir teléfono aquí");
                 sendIntent.setType("text/plain");
                 startActivity(sendIntent);
             }
