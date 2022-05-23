dd6caa45f211c31b0750b5f1387a0d2d10e26a93
Asier97am <asier.aranda.malagon@gmail.com>
2022-05-23 13:28:01 +0200

    BOTON QR LISTO

--- app/src/main/java/com/mariomunozmyaplication/gestionatunegocio/ContactosActivity.java
+++ app/src/main/java/com/mariomunozmyaplication/gestionatunegocio/ContactosActivity.java
@@ -1,11 +1,14 @@
 package com.mariomunozmyaplication.gestionatunegocio;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,11 +17,33 @@ import com.bumptech.glide.Glide;
 public class ContactosActivity extends AppCompatActivity {
 
     TextView tvGithubMario,tvGithubAsier;
+    Button btCompartirAppQR,btCompartirAppLink;
+    private ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contactos);
+
+
+        Button QR = findViewById(R.id.btCompartirAppQR);
+        Button LINK = findViewById(R.id.btCompartirAppLink);
+
+
+        QR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cargarQR(ContactosActivity.this);
+            }
+        });
+
+        LINK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
 
         //CARGAR IMAGEN DESARROLLADOR Asier
         ImageView asier = findViewById(R.id.imgAsier);
@@ -36,5 +61,32 @@ public class ContactosActivity extends AppCompatActivity {
                 .centerCrop()
                 .into(mario);
 
+
+        image = new ImageView(this);
+        image.setImageResource(R.drawable.linkgesty);
+
     }
+
+
+    public void cargarQR(ContactosActivity contactos){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Escanea el código QR");//TITULO
+        builder.setIcon(R.drawable.lupaqr);//ICONO
+
+        builder.setCancelable(false);
+        builder.setPositiveButton("Salir", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(ContactosActivity.this, ContactosActivity.class);
+                startActivity(intent);
+                dialog.dismiss();
+
+            }
+        }).setView(image);
+        builder.create().show();
+
+    }
+
+
+
 }
